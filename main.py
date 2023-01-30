9984b20d71e725d74b34d557d14854ae3fd6e587
Vitor Guirardeli Arakaki <vi.arakaki@hotmail.com>
2023-01-30 16:36:57 -0300

    code

---
+++ main.py
@@ -0,0 +1,34 @@
+import json
+
+file = open(r'teste.json')
+
+file_json = json.load(file)
+
+
+name = "teste4"
+
+value = "teste novo"
+
+verify = False
+emp = False
+
+new_json = []
+
+if len(file_json["records"]) > 0:
+    for r in file_json["records"]:
+        if name == r["name"]:
+            verify = True
+            new_j = {"name": name, "value": value}
+            new_json.append(new_j)
+        else:
+            new_json.append(r)
+else:
+    print("empty")
+
+if emp == True or verify == False:
+    file_json["records"].append({"name": name, "value": value})
+else:
+    file_json["records"] = new_json
+
+with open("teste.json", "w") as new_file:
+    json.dump(file_json, new_file)
